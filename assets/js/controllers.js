a9d87a785c2cd2cd45c3f018497ccad493838a37
briantwalter <walter.brian@gmail.com>
2014-10-10 23:35:12 -0700

    img fix and Mil temp

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -20,6 +20,15 @@ app.controller('EagleTempController', function EagleTempController($scope, $http
       alert("FATAL: could not get contact API " + status);
     });
 });
+app.controller('MilwaukeeTempController', function MilwaukeeTempController($scope, $http) {
+  $http.get('http://api.openweathermap.org/data/2.5/weather?id=5263045&units=imperial').
+    success(function(data, status, headers, config) {
+      $scope.milwaukeetemp = data.main;
+    }).
+    error(function(data, status, headers, config) {
+      alert("FATAL: could not get contact API " + status);
+    });
+});
 app.controller('SeattleTempController', function SeattleTempController($scope, $http) {
   $http.get('http://api.openweathermap.org/data/2.5/weather?id=5809844&units=imperial').
     success(function(data, status, headers, config) {
